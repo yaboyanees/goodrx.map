6eda6c24bee2e42402a500fc3c3bbc7f554a7dcc
Anees <yaboyanees@users.noreply.github.com>
2020-04-06 10:06:31 -0400

    Changed basemap

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -15,9 +15,10 @@ window.addEventListener("DOMContentLoaded", init);
 var map = L.map("map").setView([39, -98], 5);
 
 // basemap
-var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
-	attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-	minZoom: 1,
+
+var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
 	maxZoom: 19
 }).addTo(map);
 
